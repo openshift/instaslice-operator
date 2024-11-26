89db3dcb2d52d522cdef7a964755c48113b5734a
Kevin Hannon <kehannon@redhat.com>
2024-11-26 04:32:54 -0500

    fix compiler error with impossible code (#282)

--- internal/controller/instaslice_controller.go
+++ internal/controller/instaslice_controller.go
@@ -293,7 +293,7 @@ func (r *InstasliceReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 								return resultDelete, errDeletingAllocation
 							}
 							resultRemove, errRemovingFinalizer := r.removeInstaSliceFinalizer(ctx, req)
-							if errDeletingAllocation != nil {
+							if errRemovingFinalizer != nil {
 								return resultRemove, errRemovingFinalizer
 							}
 						}
