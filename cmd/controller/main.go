8ac652627d33c25eea7561ebc176ea145f5c7dbd
MohammedAbdi <mohammma@usc.edu>
2024-12-16 12:11:22 -0500

    add cutom instaslice metrics
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    update metrics
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    nit
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    update metrics
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    update
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    update deployed pod total and total processed slices metrics
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    updateMetricsAllSlotsFree
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    nits
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    update promethues
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    update deployed pod total metrics call
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    remove fake capacity file
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    update profile map extraction automation
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    update
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    Track total fit across all GPUs correctly
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    add unit tests
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    update metrics url
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    nit
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    nit
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    adjust unit tests
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    nit
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    update
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    update manifests
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    update test file
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    update compatible profiles
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    nit
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    address reviews
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    address comments
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    nit
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    remove unused code
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    remove unused code
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    optimization
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    nit
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    refactor
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    refactor
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    nit
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    optimize
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    nit
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    nit
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    refactor
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    optimize
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>
    
    update
    
    Signed-off-by: MohammedAbdi <mohammma@usc.edu>

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -59,6 +59,12 @@ func init() {
 }
 
 func main() {
+	// Log info before initializing metrics exporter
+	ctrl.Log.Info("Initializing Metrics Exporter.")
+	controller.RegisterMetrics()
+	// Log info after the metrics exporter is initialized
+	ctrl.Log.Info("Metrics Exporter Initialized.")
+
 	var metricsAddr string
 	var enableLeaderElection bool
 	var probeAddr string
