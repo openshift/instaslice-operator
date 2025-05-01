d57e4ad7c792cc3781965fec7a66ab97968413e1
Ryan Phillips <rphillips@redhat.com>
2025-05-01 13:33:04 -0500

    sync

--- hack/deploy-nvidia.sh
+++ hack/deploy-nvidia.sh
@@ -79,8 +79,6 @@ _kubectl apply -f hack/manifests/nvidia-cpu-operator.yaml
 echo "Waiting for Nvidia for ${TIMEOUT}"
 _wait_for_pods_to_exist nvidia-gpu-operator gpu-operator ${TIMEOUT}
 _kubectl wait --for condition=established --timeout=300s crd/clusterpolicies.nvidia.com
-echo "Uptime before gpu cluster policy"
-_kubectl debug $(${KUBECTL} get node -o name) -- chroot /host uptime
 _kubectl apply -f hack/manifests/gpu-cluster-policy.yaml
 sleep 10m
 _kubectl label $(${KUBECTL} get node -o name) nvidia.com/mig.config=all-enabled --overwrite
@@ -89,8 +87,6 @@ _kubectl label $(${KUBECTL} get node -o name) nvidia.com/mig.config=all-enabled 
 echo "Waiting for reboot"
 sleep 10m
 _wait_for_node ${NODE_TIMEOUT}
-echo "Uptime after _wait_for_node"
-_kubectl debug $(${KUBECTL} get node -o name) -- chroot /host uptime
 _kubectl get pods -n nvidia-gpu-operator
 # wait for the gpu cluster policy to be ready
 _kubectl wait --for=condition=ready clusterpolicy/gpu-cluster-policy --timeout=5m
