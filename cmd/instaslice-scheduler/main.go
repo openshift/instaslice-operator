d018ae6f4c044da8c2c99eb207859fcfc4a311dc
Harshal Patil <harpatil@redhat.com>
2025-05-30 17:44:50 -0400

    Note to not hand roll scheduler logic for cpu, memory and storage
    Signed-off-by: Harshal Patil <harpatil@redhat.com>

--- cmd/instaslice-scheduler/main.go
+++ cmd/instaslice-scheduler/main.go
@@ -1,36 +1,41 @@
 package main
 
 import (
-   "context"
-   "fmt"
-   "os"
+	"context"
+	"fmt"
+	"os"
 
-   "github.com/spf13/cobra"
-   "github.com/openshift/instaslice-operator/pkg/cmd/scheduler"
-   "github.com/openshift/instaslice-operator/pkg/version"
+	"github.com/openshift/instaslice-operator/pkg/cmd/scheduler"
+	"github.com/openshift/instaslice-operator/pkg/version"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-   command := NewInstasliceSchedulerCommand(context.Background())
-   if err := command.Execute(); err != nil {
-       fmt.Fprintf(os.Stderr, "%v\n", err)
-       os.Exit(1)
-   }
+	command := NewInstasliceSchedulerCommand(context.Background())
+	if err := command.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
 }
+
+// TODO - Instead of hand rolling a scheduler for cpu, memory use the
+// https://github.com/kubernetes-sigs/scheduler-plugins and only implement
+// a scheduler plugin for GPU selection.
+// https://github.com/harche/instaslice-operator/issues/3 to track this.
 
 // NewInstasliceSchedulerCommand returns a cobra command for the secondary scheduler.
 func NewInstasliceSchedulerCommand(ctx context.Context) *cobra.Command {
-   cmd := &cobra.Command{
-       Use:     "instaslice-scheduler",
-       Short:   "Instaslice secondary scheduler",
-       Version: version.Get().GitVersion,
-       Run: func(cmd *cobra.Command, args []string) {
-           if err := cmd.Help(); err != nil {
-               fmt.Printf("Unable to print help: %v", err)
-           }
-           os.Exit(1)
-       },
-   }
-   cmd.AddCommand(scheduler.NewScheduler(ctx))
-   return cmd
+	cmd := &cobra.Command{
+		Use:     "instaslice-scheduler",
+		Short:   "Instaslice secondary scheduler",
+		Version: version.Get().GitVersion,
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := cmd.Help(); err != nil {
+				fmt.Printf("Unable to print help: %v", err)
+			}
+			os.Exit(1)
+		},
+	}
+	cmd.AddCommand(scheduler.NewScheduler(ctx))
+	return cmd
 }
