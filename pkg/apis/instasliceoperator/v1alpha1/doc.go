2a996b52a0145c7f40c7b929e192b4cdb9a84d30
Harshal Patil <harpatil@redhat.com>
2025-06-16 15:30:42 -0500

    Remove pkg/apis/instasliceoperator
    Signed-off-by: Harshal Patil <harpatil@redhat.com>

--- pkg/apis/instasliceoperator/v1alpha1/doc.go
+++
@@ -1,4 +0,0 @@
-// Package v1alpha1 contains API Schema definitions for the OpenShiftOperator v1 API group
-// Compatibility aliases for the old instasliceoperator API group.
-// +groupName=inference.redhat.com
-package v1alpha1
