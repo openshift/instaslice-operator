c49232c889adfe9ac50caa13db3a3ffd7508d5cd
Harshal Patil <harpatil@redhat.com>
2025-06-17 08:11:37 -0500

    basic e2e tests
    Signed-off-by: Harshal Patil <harpatil@redhat.com>

---
+++ test/e2e/test_pod_test.go
@@ -0,0 +1,130 @@
+package e2e
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	clientset "github.com/openshift/instaslice-operator/pkg/generated/clientset/versioned"
+
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var (
+	kubeClient *kubernetes.Clientset
+	dasClient  *clientset.Clientset
+)
+
+const (
+	testNamespace = "das-e2e"
+)
+
+var _ = BeforeSuite(func() {
+	cfg, err := rest.InClusterConfig()
+	if err != nil {
+		kubeconfig := os.Getenv("KUBECONFIG")
+		if kubeconfig == "" {
+			kubeconfig = filepath.Join(os.Getenv("HOME"), ".kube", "config")
+		}
+		cfg, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			Skip("kubernetes config not available: " + err.Error())
+		}
+	}
+	kubeClient, err = kubernetes.NewForConfig(cfg)
+	if err != nil {
+		Skip("failed to create kubernetes client: " + err.Error())
+	}
+	dasClient, err = clientset.NewForConfig(cfg)
+	if err != nil {
+		Skip("failed to create das client: " + err.Error())
+	}
+})
+
+var _ = Describe("Test Pod from deploy-k8s", Ordered, func() {
+	var (
+		podNames  []string
+		namespace string
+	)
+
+	BeforeAll(func() {
+		if os.Getenv("KUBECONFIG") == "" {
+			Skip("KUBECONFIG is not set; skipping e2e test")
+		}
+	})
+
+	BeforeAll(func() {
+		namespace = testNamespace
+
+		ns := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace}}
+		By("creating namespace " + namespace)
+		_, err := kubeClient.CoreV1().Namespaces().Create(context.Background(), ns, metav1.CreateOptions{})
+		if err != nil && !apierrors.IsAlreadyExists(err) {
+			Expect(err).NotTo(HaveOccurred())
+		}
+
+		podSpec := corev1.PodSpec{
+			SchedulerName: "das-scheduler",
+			Containers: []corev1.Container{ // single-container pod
+				{
+					Name:    "busy",
+					Image:   "quay.io/prometheus/busybox",
+					Command: []string{"sh", "-c", "sleep 3600"},
+					Resources: corev1.ResourceRequirements{
+						Limits: corev1.ResourceList{
+							corev1.ResourceName("mig.das.com/1g.5gb"): resource.MustParse("1"),
+						},
+					},
+				},
+			},
+		}
+
+		for i := 1; i <= 14; i++ {
+			pod := &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      fmt.Sprintf("test-das-%d", i),
+					Namespace: namespace,
+				},
+				Spec: podSpec,
+			}
+
+			By("creating test pod " + pod.Name)
+			_, err := kubeClient.CoreV1().Pods(namespace).Create(context.Background(), pod, metav1.CreateOptions{})
+			Expect(err).NotTo(HaveOccurred())
+			podNames = append(podNames, pod.Name)
+		}
+	})
+
+	AfterAll(func() {
+		By("deleting namespace " + namespace)
+		err := kubeClient.CoreV1().Namespaces().Delete(context.Background(), namespace, metav1.DeleteOptions{})
+		Expect(err).NotTo(HaveOccurred())
+
+		Eventually(func() bool {
+			_, err := kubeClient.CoreV1().Namespaces().Get(context.Background(), namespace, metav1.GetOptions{})
+			return apierrors.IsNotFound(err)
+		}, 1*time.Minute, time.Second).Should(BeTrue())
+	})
+
+	It("should be running", func(ctx SpecContext) {
+		for _, name := range podNames {
+			Eventually(func() (corev1.PodPhase, error) {
+				p, err := kubeClient.CoreV1().Pods(namespace).Get(ctx, name, metav1.GetOptions{})
+				if err != nil {
+					return "", err
+				}
+				return p.Status.Phase, nil
+			}, 2*time.Minute, 5*time.Second).Should(Equal(corev1.PodRunning))
+		}
+	})
+})
