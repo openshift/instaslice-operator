fb6d4b8002c9ff58ee0c5c12d0e5ff79feff276d
Harshal Patil <harpatil@redhat.com>
2025-07-23 15:55:37 -0400

    Add deployment in e2e
    Signed-off-by: Harshal Patil <harpatil@redhat.com>

--- test/e2e/e2e.go
+++ test/e2e/e2e.go
@@ -14,6 +14,7 @@ import (
 	instav1 "github.com/openshift/instaslice-operator/pkg/apis/dasoperator/v1alpha1"
 	clientset "github.com/openshift/instaslice-operator/pkg/generated/clientset/versioned"
 
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -70,9 +71,10 @@ var (
 )
 
 const (
-	testNamespace          = "das-e2e"
-	multiTestNamespace     = "das-e2e-multi"
-	multiResourceNamespace = "das-e2e-multires"
+	testNamespace           = "das-e2e"
+	multiTestNamespace      = "das-e2e-multi"
+	multiResourceNamespace  = "das-e2e-multires"
+	deploymentTestNamespace = "das-e2e-deploy"
 )
 
 var _ = BeforeSuite(func() {
@@ -158,6 +160,7 @@ var _ = Describe("Test pods for requesting single type of extended resource", Or
 		for _, p := range pods {
 			podNames = append(podNames, p.Name)
 		}
+		Expect(podNames).NotTo(BeEmpty())
 	})
 
 	AfterAll(func() {
@@ -172,6 +175,7 @@ var _ = Describe("Test pods for requesting single type of extended resource", Or
 	})
 
 	It("should be running", func(ctx SpecContext) {
+		Expect(podNames).NotTo(BeEmpty())
 		for _, name := range podNames {
 			Eventually(func() (corev1.PodPhase, error) {
 				p, err := kubeClient.CoreV1().Pods(namespace).Get(ctx, name, metav1.GetOptions{})
@@ -184,6 +188,7 @@ var _ = Describe("Test pods for requesting single type of extended resource", Or
 	})
 
 	It("should create allocationclaims for each requested GPU slice", func(ctx SpecContext) {
+		Expect(podNames).NotTo(BeEmpty())
 		expected := 0
 		for _, name := range podNames {
 			p, err := kubeClient.CoreV1().Pods(namespace).Get(ctx, name, metav1.GetOptions{})
@@ -204,28 +209,11 @@ var _ = Describe("Test pods for requesting single type of extended resource", Or
 		}, 2*time.Minute, 5*time.Second).Should(Equal(expected))
 	})
 
-	It("should set NVIDIA_VISIBLE_DEVICES env var in each pod", func(ctx SpecContext) {
+	It("should set NVIDIA_VISIBLE_DEVICES and CUDA_VISIBLE_DEVICES env vars in each pod", func(ctx SpecContext) {
+		Expect(podNames).NotTo(BeEmpty())
 		for _, name := range podNames {
 			Eventually(func() (bool, error) {
-				req := kubeClient.CoreV1().Pods(namespace).GetLogs(name, &corev1.PodLogOptions{})
-				out, err := req.Do(ctx).Raw()
-				if err != nil {
-					return false, err
-				}
-				return strings.Contains(string(out), "NVIDIA_VISIBLE_DEVICES="), nil
-			}, 2*time.Minute, 5*time.Second).Should(BeTrue())
-		}
-	})
-
-	It("should set CUDA_VISIBLE_DEVICES env var in each pod", func(ctx SpecContext) {
-		for _, name := range podNames {
-			Eventually(func() (bool, error) {
-				req := kubeClient.CoreV1().Pods(namespace).GetLogs(name, &corev1.PodLogOptions{})
-				out, err := req.Do(ctx).Raw()
-				if err != nil {
-					return false, err
-				}
-				return strings.Contains(string(out), "CUDA_VISIBLE_DEVICES="), nil
+				return verifyVisibleDevicesEnv(ctx, namespace, name, 0)
 			}, 2*time.Minute, 5*time.Second).Should(BeTrue())
 		}
 	})
@@ -249,6 +237,7 @@ var _ = Describe("Test pods for requesting single type of extended resource", Or
 			}
 			return p.Status.Phase, nil
 		}, 25*time.Second, 5*time.Second).Should(Equal(corev1.PodPending))
+		Expect(podNames).NotTo(BeEmpty())
 		By("deleting pod " + podNames[0])
 		err := kubeClient.CoreV1().Pods(namespace).Delete(ctx, podNames[0], metav1.DeleteOptions{})
 		Expect(err).NotTo(HaveOccurred())
@@ -265,6 +254,124 @@ var _ = Describe("Test pods for requesting single type of extended resource", Or
 			}
 			return p.Status.Phase, nil
 		}, 60*time.Minute, 5*time.Second).Should(Equal(corev1.PodRunning))
+	})
+})
+
+var _ = Describe("Test deployment requesting single type of extended resource", Ordered, func() {
+	var (
+		podNames   []string
+		namespace  string
+		deployName string
+	)
+
+	BeforeAll(func() {
+		if os.Getenv("KUBECONFIG") == "" {
+			Skip("KUBECONFIG is not set; skipping e2e test")
+		}
+	})
+
+	BeforeAll(func() {
+		namespace = deploymentTestNamespace
+		deployName = "cuda-vectoradd"
+
+		ns := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace}}
+		By("creating namespace " + namespace)
+		_, err := kubeClient.CoreV1().Namespaces().Create(context.Background(), ns, metav1.CreateOptions{})
+		if err != nil && !apierrors.IsAlreadyExists(err) {
+			Expect(err).NotTo(HaveOccurred())
+		}
+
+		dep := &appsv1.Deployment{
+			ObjectMeta: metav1.ObjectMeta{Name: deployName, Namespace: namespace},
+			Spec: appsv1.DeploymentSpec{
+				Replicas: pointer.Int32(2),
+				Selector: &metav1.LabelSelector{MatchLabels: map[string]string{"app": deployName}},
+				Template: corev1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"app": deployName}},
+					Spec: func() corev1.PodSpec {
+						spec := defaultGPUSlicePodSpec()
+						spec.RestartPolicy = corev1.RestartPolicyAlways
+						return spec
+					}(),
+				},
+			},
+		}
+
+		By("creating deployment")
+		_, err = kubeClient.AppsV1().Deployments(namespace).Create(context.Background(), dep, metav1.CreateOptions{})
+		Expect(err).NotTo(HaveOccurred())
+
+		Eventually(func() ([]string, error) {
+			pl, err := kubeClient.CoreV1().Pods(namespace).List(context.Background(), metav1.ListOptions{LabelSelector: "app=" + deployName})
+			if err != nil {
+				return nil, err
+			}
+			if len(pl.Items) < 2 {
+				return nil, fmt.Errorf("waiting")
+			}
+			names := make([]string, len(pl.Items))
+			for i := range pl.Items {
+				names[i] = pl.Items[i].Name
+			}
+			podNames = names
+			return names, nil
+		}, 2*time.Minute, 5*time.Second).Should(HaveLen(2))
+		Expect(podNames).NotTo(BeEmpty())
+	})
+
+	AfterAll(func() {
+		By("deleting namespace " + namespace)
+		err := kubeClient.CoreV1().Namespaces().Delete(context.Background(), namespace, metav1.DeleteOptions{})
+		Expect(err).NotTo(HaveOccurred())
+
+		Eventually(func() bool {
+			_, err := kubeClient.CoreV1().Namespaces().Get(context.Background(), namespace, metav1.GetOptions{})
+			return apierrors.IsNotFound(err)
+		}, 2*time.Minute, time.Second).Should(BeTrue())
+	})
+
+	It("should be running", func(ctx SpecContext) {
+		Expect(podNames).NotTo(BeEmpty())
+		for _, name := range podNames {
+			Eventually(func() (corev1.PodPhase, error) {
+				p, err := kubeClient.CoreV1().Pods(namespace).Get(ctx, name, metav1.GetOptions{})
+				if err != nil {
+					return "", err
+				}
+				return p.Status.Phase, nil
+			}, 60*time.Minute, 5*time.Second).Should(Equal(corev1.PodRunning))
+		}
+	})
+
+	It("should create allocationclaims for each requested GPU slice", func(ctx SpecContext) {
+		Expect(podNames).NotTo(BeEmpty())
+		expected := 0
+		for _, name := range podNames {
+			p, err := kubeClient.CoreV1().Pods(namespace).Get(ctx, name, metav1.GetOptions{})
+			Expect(err).NotTo(HaveOccurred())
+			for _, c := range p.Spec.Containers {
+				if q, ok := c.Resources.Limits[corev1.ResourceName("mig.das.com/1g.5gb")]; ok {
+					expected += int(q.Value())
+				}
+			}
+		}
+
+		Eventually(func() (int, error) {
+			allocs, err := dasClient.OpenShiftOperatorV1alpha1().AllocationClaims("das-operator").List(ctx, metav1.ListOptions{})
+			if err != nil {
+				return 0, err
+			}
+			return len(allocs.Items), nil
+		}, 2*time.Minute, 5*time.Second).Should(Equal(expected))
+	})
+
+	It("should set NVIDIA_VISIBLE_DEVICES and CUDA_VISIBLE_DEVICES env vars in each pod", func(ctx SpecContext) {
+		Expect(podNames).NotTo(BeEmpty())
+		for _, name := range podNames {
+			Eventually(func() (bool, error) {
+				return verifyVisibleDevicesEnv(ctx, namespace, name, 0)
+			}, 2*time.Minute, 5*time.Second).Should(BeTrue())
+		}
 	})
 })
 
@@ -315,6 +422,7 @@ var _ = Describe("Test pods requesting multiple resources", Ordered, func() {
 		for _, p := range pods {
 			podNames = append(podNames, p.Name)
 		}
+		Expect(podNames).NotTo(BeEmpty())
 	})
 
 	AfterAll(func() {
@@ -329,6 +437,7 @@ var _ = Describe("Test pods requesting multiple resources", Ordered, func() {
 	})
 
 	It("should be running", func(ctx SpecContext) {
+		Expect(podNames).NotTo(BeEmpty())
 		for _, name := range podNames {
 			Eventually(func() (corev1.PodPhase, error) {
 				p, err := kubeClient.CoreV1().Pods(namespace).Get(ctx, name, metav1.GetOptions{})
@@ -340,41 +449,12 @@ var _ = Describe("Test pods requesting multiple resources", Ordered, func() {
 		}
 	})
 
-	It("should set NVIDIA_VISIBLE_DEVICES to 3 comma separated values", func(ctx SpecContext) {
+	It("should set NVIDIA_VISIBLE_DEVICES and CUDA_VISIBLE_DEVICES to 3 comma separated values", func(ctx SpecContext) {
+		Expect(podNames).NotTo(BeEmpty())
 		for _, name := range podNames {
-			Eventually(func() (int, error) {
-				req := kubeClient.CoreV1().Pods(namespace).GetLogs(name, &corev1.PodLogOptions{})
-				out, err := req.Do(ctx).Raw()
-				if err != nil {
-					return 0, err
-				}
-				for _, line := range strings.Split(string(out), "\n") {
-					if strings.HasPrefix(line, "NVIDIA_VISIBLE_DEVICES=") {
-						val := strings.TrimPrefix(line, "NVIDIA_VISIBLE_DEVICES=")
-						return len(strings.Split(strings.TrimSpace(val), ",")), nil
-					}
-				}
-				return 0, fmt.Errorf("env var not found")
-			}, 2*time.Minute, 5*time.Second).Should(Equal(3))
-		}
-	})
-
-	It("should set CUDA_VISIBLE_DEVICES to 3 comma separated values", func(ctx SpecContext) {
-		for _, name := range podNames {
-			Eventually(func() (int, error) {
-				req := kubeClient.CoreV1().Pods(namespace).GetLogs(name, &corev1.PodLogOptions{})
-				out, err := req.Do(ctx).Raw()
-				if err != nil {
-					return 0, err
-				}
-				for _, line := range strings.Split(string(out), "\n") {
-					if strings.HasPrefix(line, "CUDA_VISIBLE_DEVICES=") {
-						val := strings.TrimPrefix(line, "CUDA_VISIBLE_DEVICES=")
-						return len(strings.Split(strings.TrimSpace(val), ",")), nil
-					}
-				}
-				return 0, fmt.Errorf("env var not found")
-			}, 2*time.Minute, 5*time.Second).Should(Equal(3))
+			Eventually(func() (bool, error) {
+				return verifyVisibleDevicesEnv(ctx, namespace, name, 3)
+			}, 2*time.Minute, 5*time.Second).Should(BeTrue())
 		}
 	})
 })
@@ -439,6 +519,7 @@ var _ = Describe("Test pods for requesting multiple slice types", Ordered, func(
 		for _, p := range pods {
 			podNames = append(podNames, p.Name)
 		}
+		Expect(podNames).NotTo(BeEmpty())
 	})
 
 	AfterAll(func() {
@@ -453,6 +534,7 @@ var _ = Describe("Test pods for requesting multiple slice types", Ordered, func(
 	})
 
 	It("should be running", func(ctx SpecContext) {
+		Expect(podNames).NotTo(BeEmpty())
 		for _, name := range podNames {
 			Eventually(func() (corev1.PodPhase, error) {
 				p, err := kubeClient.CoreV1().Pods(namespace).Get(ctx, name, metav1.GetOptions{})
@@ -473,4 +555,34 @@ func createPods(ctx context.Context, namespace string, pods []*corev1.Pod) error
 		}
 	}
 	return nil
+}
+
+func verifyVisibleDevicesEnv(ctx context.Context, namespace, podName string, expectedCount int) (bool, error) {
+	req := kubeClient.CoreV1().Pods(namespace).GetLogs(podName, &corev1.PodLogOptions{})
+	out, err := req.Do(ctx).Raw()
+	if err != nil {
+		return false, err
+	}
+
+	var nvidia, cuda string
+	for _, line := range strings.Split(string(out), "\n") {
+		if strings.HasPrefix(line, "NVIDIA_VISIBLE_DEVICES=") {
+			nvidia = strings.TrimPrefix(line, "NVIDIA_VISIBLE_DEVICES=")
+		}
+		if strings.HasPrefix(line, "CUDA_VISIBLE_DEVICES=") {
+			cuda = strings.TrimPrefix(line, "CUDA_VISIBLE_DEVICES=")
+		}
+	}
+
+	if nvidia == "" || cuda == "" || nvidia != cuda {
+		return false, nil
+	}
+
+	if expectedCount > 0 {
+		if len(strings.Split(strings.TrimSpace(nvidia), ",")) != expectedCount {
+			return false, nil
+		}
+	}
+
+	return true, nil
 }
